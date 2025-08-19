c6894686d9aa6f283fe1f94ac7fc5ffd970f8d49
vicso-name <vic.sokoliuk@gmail.com>
2025-08-19 09:32:47 +0300

    replace dynamic IconData with constants for tree shaking

--- lib/models/mood.dart
+++ lib/models/mood.dart
@@ -4,14 +4,14 @@ import '../constants/moods.dart';
 class Mood {
   final MoodType type;
   final String name;
-  final IconData icon; // Заменено emoji на icon
+  final IconData icon;
   final int color;
   final String searchKey;
 
   Mood({
     required this.type,
     required this.name,
-    required this.icon, // Заменено emoji на icon
+    required this.icon,
     required this.color,
     required this.searchKey,
   });
@@ -30,25 +30,29 @@ class Mood {
     return {
       'type': type.name,
       'name': name,
-      'icon_code_point': icon.codePoint, // Сохраняем codePoint иконки
+      'icon_code_point': icon.codePoint,
       'color': color,
       'search_key': searchKey,
     };
   }
 
   factory Mood.fromJson(Map<String, dynamic> json) {
+    final moodType = MoodType.values.firstWhere(
+      (e) => e.name == json['type'],
+      orElse: () => MoodType.happy,
+    );
+
+    final moodData = Moods.all.firstWhere(
+      (mood) => mood.type == moodType,
+      orElse: () => Moods.all.first,
+    );
+
     return Mood(
-      type: MoodType.values.firstWhere(
-        (e) => e.name == json['type'],
-        orElse: () => MoodType.happy,
-      ),
-      name: json['name'] ?? '',
-      icon: IconData(
-        json['icon_code_point'] ?? 0, // Создаём IconData из codePoint
-        fontFamily: 'MaterialIcons',
-      ),
-      color: json['color'] ?? 0,
-      searchKey: json['search_key'] ?? 'happy',
+      type: moodType,
+      name: json['name'] ?? moodData.name,
+      icon: moodData.icon,
+      color: json['color'] ?? moodData.color.toARGB32(),
+      searchKey: json['search_key'] ?? moodData.searchKey,
     );
   }
 }
